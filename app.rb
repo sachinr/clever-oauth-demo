fc038c057b79d166bb91a1abf78e5e3da8c8dff7
Sachin Ranchod <sachin.ranchod@gmail.com>
2016-06-19 12:22:55 -0700

    print

--- app.rb
+++ app.rb
@@ -6,7 +6,15 @@ get '/' do
   HTML
 end
 
+get '/success' do
+  'success'
+end
+
 post '/auth/:name/callback' do
   auth = request.env['omniauth.auth']
+  session[:uid] = env['omniauth.auth']['uid']
+  # this is the main endpoint to your application
+  # redirect to('/')
+  'hello'
   # do whatever you want with the information!
 end
